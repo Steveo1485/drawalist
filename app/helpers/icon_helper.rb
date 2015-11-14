eaa9129b2a41c03e51201c5c6b3aded86b13fcee
Steven Nugent <stevennugent@gmail.com>
2015-11-14 12:47:07 -0800

    Add font-awesome, create IconHelper

---
+++ app/helpers/icon_helper.rb
@@ -0,0 +1,23 @@
+module IconHelper
+
+  def gift_icon(options = {})
+    icon('gift', options)
+  end
+
+  def group_icon(options = {})
+    icon('users', options)
+  end
+
+  def list_icon(options = {})
+    icon('list', options)
+  end
+
+  def star_icon(options = {})
+    icon('star', options)
+  end
+
+  def tree_icon(options = {})
+    icon('tree', options)
+  end
+
+end
