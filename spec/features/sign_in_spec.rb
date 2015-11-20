619d38434629e144b0dc55a3f13364c56caa85e8
Steven Nugent <stevennugent@gmail.com>
2015-11-19 22:05:57 -0800

    User log_in_user helper

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -7,10 +7,7 @@ describe "The sign in process", :type => :feature do
   end
 
   it "should redirect to user dashboard after successful sign in" do
-    visit root_path
-    fill_in 'user_email', with: @user.email
-    fill_in 'user_password', with: @user.password
-    click_button('Sign In')
+    log_in_user(@user)
     expect(current_path).to eq(user_root_path)
   end
 
