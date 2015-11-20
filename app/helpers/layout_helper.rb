5844eae0d385d2e6ae09c70576596d33c5d1dabd
Steven Nugent <stevennugent@gmail.com>
2015-11-19 21:39:13 -0800

    Add title helper

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -8,4 +8,16 @@ module LayoutHelper
     "#{params[:controller]}-#{params[:action]}"
   end
 
+  def title(page_title)
+    content_for(:title) { raw(page_title.to_s) }
+  end
+
+  def compound_page_title(page_title)
+    title = ''
+    unless page_title.blank?
+      title += "#{page_title} - "
+    end
+    title += 'DrawAList'
+  end
+
 end
