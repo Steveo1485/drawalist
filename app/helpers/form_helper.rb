134e2005ceb5025a76e17789cf161fc372687c76
Steven Nugent <stevennugent@gmail.com>
2015-11-20 21:09:21 -0800

    Update handle_errors helper

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,7 +1,9 @@
 module FormHelper
 
   def handle_errors(object)
-    flash.now[:alert] = raw(object.errors.full_messages.join('<br />'))
+    if object.errors.any?
+      flash.now[:alert] = raw(object.errors.full_messages.join('<br />'))
+    end
   end
 
 end
